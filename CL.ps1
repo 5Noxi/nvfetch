4a31a4ce3836f1d16aa42a43abc13ec40ddd899a
Nohuxi <noel.ballier@gmail.com>
2025-11-04 16:05:43 +0100

    ...

--- CL.ps1
+++ CL.ps1
@@ -2,10 +2,11 @@ $dir = "$env:APPDATA\Noverse"
 $dest = "$env:APPDATA\Noverse\NVFetch.ps1"
 
 if (!(Test-Path $dir)) { ni -ItemType Directory -Path $dir -Force | Out-Null }
-mv "$HOME\Downloads\NVFetch.ps1" -Destination $dest -Force
+mv "$env:USERPROFILE\Downloads\NVFetch.ps1" -Destination $dest -Force
 
 if (!(Test-Path $profile)) { ni -ItemType File -Path $profile -Force | Out-Null }
 ac $profile -Value "`nfunction nvfetch { & `"$dest`" @args }"
 
 . $profile
+
 
